b36c66f5c7248f4688bd7b2c43b5b4be189b3ea5
astrix <example@email.com>
2023-03-09 16:02:45 +0100

    add schema for users and accounts tables

---
+++ lib/real_deal_api/users/user.ex
@@ -0,0 +1,23 @@
+defmodule RealDealApi.Users.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "users" do
+    field :biography, :string
+    field :full_name, :string
+    field :gender, :string
+    # field :account_id, :binary_id
+    belongs_to :account, RealDealApi.Accounts.Account
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:account_id, :full_name, :gender, :biography])
+    |> validate_required([:account_id])
+  end
+end
